b927a47ddd0492a0e35b5194cf1aa2eb8a2ea6e5
Tiago Selpuveda <tiagofoks@yahoo.com.br>
2025-11-20 20:47:04 -0300

    Add sh-pages and prepare pkg to deploy

--- orcamento/next.config.js
+++ orcamento/next.config.js
@@ -3,6 +3,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // ... other config settings
+  output: 'export', // Isso força o build a ser 100% estático
   experimental: {
     turbopack: {
       root: './dev/orcamento', // Set this to your actual project directory
